de26a7db494d6bbc7e0ef934e34c8de8a91917da
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-10-29 12:35:13 +0400

    wip: temporary commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const github = require('@actions/github') // docs: https://github.com/actions/to
 const input = {
   githubToken: core.getInput('github-token', {required: true}),
   username: core.getInput('username', {required: true}), // eg. octocat
-  repository: core.getInput('repository', {required: true}), // eg. octocat/Hello-World
+  repository: core.getInput('repository', {required: true}).toLowerCase(), // eg. octocat/Hello-World
 }
 
 // main action entrypoint
@@ -14,73 +14,36 @@ async function runAction() {
   const perPage = 100
   let result = false
 
+  core.startGroup('⭐ Fetching stars...')
+
   for (let pageNum = 0; ; pageNum++) {
+    core.debug(`Request stars page #${pageNum}...`)
 
     const resp = await octokit.request('GET /users/{username}/starred', {
       username: input.username,
       per_page: perPage,
       page: pageNum,
-    });
+    })
 
-    console.log(resp)
+    core.debug(`${resp.data.length} repositories found in the response`)
 
-    if (resp.data.length < perPage) {
-      break;
+    if (resp.data.some(repo => repo.full_name.toLowerCase() === input.repository)) {
+      result = true
+
+      break
     }
 
-    break;
+    if (resp.data.length < perPage) {
+      break
+    }
   }
 
+  core.endGroup()
+
+  core.info('Star was' + (result ? ' ' : ' not ') + 'found')
+
   core.setOutput('is-stargazer', result)
-
-  // // docs: https://octokit.github.io/rest.js/v18#repos-get-latest-release
-  // const latest = await octokit.rest.repos.getLatestRelease({
-  //   owner: 'StackExchange',
-  //   repo: 'dnscontrol',
-  // })
-  //
-  // return latest.data.tag_name.replace(/^v/, '') // strip the 'v' prefix
-  //
-  // core.setOutput('dnscontrol-bin', dnscontrolBinPath)
-  //
-  // let version
-  //
-  // if (input.version.toLowerCase() === 'latest') {
-  //   core.debug('Requesting latest DNSControl version...')
-  //   version = await getLatestDNSControlVersion(input.githubToken)
-  // } else {
-  //   version = input.version
-  // }
-  //
-  // core.startGroup('💾 Install DNSControl')
-  // await doInstall(version)
-  // core.endGroup()
-  //
-  // core.startGroup('🧪 Installation check')
-  // await doCheck()
-  // core.endGroup()
 }
-
-// /**
-//  * @param {octokit.Octokit} octokit
-//  * @param {string} user
-//  */
-// async function isStarredBy(octokit, user) {
-//   for (let page = 0; ; page++) {
-//     const resp = await octokit.request('GET /repos/{owner}/{repo}/stargazers', {
-//       ...github.context.repo,
-//       page,
-//       per_page: 100
-//     });
-//     if (resp.data.some(u => u.login === user)) {
-//       return true;
-//     }
-//     if (resp.data.length < 100) {
-//       break;
-//     }
-//   }
-//   return false;
-// }
 
 // run the action
 (async () => {
